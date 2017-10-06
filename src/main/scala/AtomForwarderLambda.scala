cfad5795675e710155a004334efd9acd41f6bfb4
Andy Gallagher <andy.gallagher@theguardian.com>
2017-10-06 12:29:59 +0100

    updating to deploy to content-api account, logging improvement, correct trigger for cloudformation

--- src/main/scala/AtomForwarderLambda.scala
+++ src/main/scala/AtomForwarderLambda.scala
@@ -15,6 +15,21 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Try
 
 class AtomForwarderLambda extends RequestHandler[KinesisEvent, Unit] {
+  def itemTypeAsString(itemType: ItemType): String = itemType match {
+    case ItemType.Content=>"Content"
+    case ItemType.Section=>"Section"
+    case ItemType.Tag=>"Tag"
+    case ItemType.StoryPackage=>"Story Package"
+    case _=>"unknown"
+  }
+
+  def eventTypeAsString(eventType: EventType):String = eventType match {
+    case EventType.Update=>"Update"
+    case EventType.Delete=>"Delete"
+    case EventType.RetrievableUpdate=>"Retrievable Update"
+    case _=>"unknown"
+  }
+
   override def handleRequest(event:KinesisEvent, context: Context): Unit = {
     val rawRecords: List[Record] = event.getRecords.asScala.map(_.getKinesis).toList
     val userRecords = UserRecord.deaggregate(rawRecords.asJava)
@@ -25,6 +40,7 @@ class AtomForwarderLambda extends RequestHandler[KinesisEvent, Unit] {
         case ItemType.Atom=>
           event.payload.map({
             case EventPayload.Atom(atom)=>
+              println("Got an atom payload, forwarding on...")
               Future.sequence(Seq(
                 SNSHandler.tellSNS(event),
                 KinesisHandler.tellKinesis(record)
@@ -36,7 +52,7 @@ class AtomForwarderLambda extends RequestHandler[KinesisEvent, Unit] {
               Future(false)
           }).getOrElse(Future(false))
         case _=>
-          println("This event is for something other than an atom, not going to do anything.")
+          println(s"This event is for a ${itemTypeAsString(event.itemType)} ${eventTypeAsString(event.eventType)}, not going to do anything.")
           Future(false)
       }
     }.onComplete({ totalProcessed:Try[Int]=>
