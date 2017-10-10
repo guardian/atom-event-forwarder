5159b5e0b5465d66f93bd6778300861c916da70b
Andy Gallagher <andy.gallagher@theguardian.com>
2017-10-10 11:39:03 +0100

    removing test for content events as not using them now

--- src/test/scala/TestSNSHandler.scala
+++ src/test/scala/TestSNSHandler.scala
@@ -26,24 +26,4 @@ class TestSNSHandler extends FunSuite with Matchers with Logging {
     jsonString should equal("{\"payloadId\":\"fakeIdString\",\"eventType\":{\"Update\":{}},\"itemType\":{\"Atom\":{}},\"dateTime\":1507221293,\"payload\":{\"atom\":{\"id\":\"someId\",\"atomType\":\"media\",\"labels\":[],\"defaultHtml\":\"<p></p>\",\"data\":{\"media\":{\"assets\":[{\"assetType\":\"audio\",\"version\":1,\"id\":\"SomeAssetId\",\"platform\":\"url\"}],\"title\":\"some title\",\"category\":\"news\"}},\"contentChangeDetails\":{\"revision\":1},\"commissioningDesks\":[]}}}")
   }
 
-  test("eventToJson should serialize a content event"){
-
-    //val contentItem = Content(id="someFakeId",ContentType.Article)
-
-    val contentItem = Content(id="someFakeId",ContentType.Article,webTitle="some web title",webUrl="http://path/to/web",apiUrl="http://path/to/api")
-
-    val payload = EventPayload.Content(contentItem.asInstanceOf[EventPayloadAliases.ContentAlias])
-
-    val event = Event(payloadId = "fakeIdString",
-      eventType = EventType.Update,
-      itemType = ItemType.Content,
-      dateTime = 1507221293,
-      payload = Some(payload)
-    )
-
-    val maybeJson = SNSHandler.eventToJson(event)
-    maybeJson should be(defined)
-    val jsonString = maybeJson.get
-    jsonString should equal("{\"payloadId\":\"fakeIdString\",\"eventType\":{\"Update\":{}},\"itemType\":{\"Content\":{}},\"dateTime\":1507221293,\"payload\":{\"content\":{\"id\":\"someFakeId\",\"type\":\"article\",\"webTitle\":\"some web title\",\"webUrl\":\"http://path/to/web\",\"apiUrl\":\"http://path/to/api\",\"tags\":[],\"references\":[],\"isHosted\":false}}}")
-  }
 }
